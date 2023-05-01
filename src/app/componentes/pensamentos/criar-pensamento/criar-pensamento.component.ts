204b4035a9f1f5824d3278e9244052bb3b3e87c2
taisbelucik <taisbelucik@hotmail.com>
2023-05-01 18:45:04 -0300

    adicioanndo previa dos pensamentos

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
-export class CriarPensamentoComponent implements OnInit {
+export class CriarPensamentoComponent implements OnInit  {
 
   pensamento = {
+    id: "1",
+    conteudo: "Aprendendo Angular",
+    autoria: "Dev",
+    modelo: ""
 
   }
 
+constructor() {}
 
-  constructor () {}
-  ngOnInit(): void {
+ngOnInit(): void {
 
-  }
 }
+
+criarPensamento() {
+  alert("Novo pensamento criado")
+}
+
+cancelar(){
+  alert("pensamento exluido")
+}
+  }
+
