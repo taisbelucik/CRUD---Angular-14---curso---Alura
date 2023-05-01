52d1edacd77e6f9698e2064707a4b322f4893c71
taisbelucik <taisbelucik@hotmail.com>
2023-05-01 15:23:34 -0300

    adicionando formulário

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+
+  }
+
+
+  constructor () {}
+  ngOnInit(): void {
+
+  }
+}
